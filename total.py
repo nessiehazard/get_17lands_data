caf9053a79e2cd47ca6e0cee83900bc9ddf5c2d4
nessiehazard <76423628+nessiehazard@users.noreply.github.com>
2023-12-28 15:24:31 +0900

    17landsでの待機時間をクリック可能になるまで待機に変更

--- total.py
+++ total.py
@@ -1,10 +1,14 @@
 import pandas as pd
 from selenium import webdriver
-import time # ダウンロード待ちや動的なページの読み込み待ち(いずれは暗黙的な感じでやりたい)
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time # ダウンロード待ちや動的なページの読み込み待ち(いずれは暗黙的な感じでやりたい)
 import re
 import os
 from glob import glob
+
+
 
 
 
@@ -34,34 +38,40 @@ downloaddir = os.getcwd()+ "\\downloads"
 options = webdriver.ChromeOptions()
 options.add_experimental_option("prefs", {"download.default_directory": downloaddir})
 
-# ブラウザの起動
 
-driver = webdriver.Chrome(options)
+# WebDriverのパスを指定してChromeを起動
+driver = webdriver.Chrome()
 
-driver.get('https://www.17lands.com/card_data')
+# ウェブページにアクセス
+url = 'https://www.17lands.com/card_data'
+driver.get(url)
+# pack_nameが取得可能になるまで待機 (適切なセレクタを使用してください)
+pack_name_button_selector = '#expansion > option:nth-child(1)'
+wait = WebDriverWait(driver, 10)
+pack_name_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, pack_name_button_selector)))
+pack_name = pack_name_button.text
 
-time.sleep(5)
-element = driver.find_element(By.XPATH, "//*[@id=\"expansion\"]/option[1]")
-pack_name = element.text
+print(pack_name)
 
-time.sleep(5)
+# Export_dataボタンがクリック可能になるまで待機 (適切なセレクタを使用してください)
+Export_data_button_selector = '#app > div > div.sc-eywOmQ.fDjuIB > div.ui.dropdown > div.divider.text'
+Export_data_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, Export_data_button_selector)))
 
-# Export　data部分の要素を取得
-element = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]')
-# Export　dataをクリック
-element.click()
+# Export_dataボタンをクリック
+Export_data_button.click()
 
-time.sleep(5)
+# Download_csvボタンがクリック可能になるまで待機 (適切なセレクタを使用してください)
+Download_csv_button_selector = '#app > div > div.sc-eywOmQ.fDjuIB > div.ui.active.visible.dropdown > div.menu.transition.visible > div:nth-child(2) > a'
+Download_csv_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, Download_csv_button_selector)))
 
-# Download as CSV部分の要素を取得
-element_csv = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[2]/div[1]/div[2]/div[2]/a')
-# Download as CSVをクリック
-element_csv.click()
+# Download_csvボタンをクリック
+Download_csv_button.click()
 
+time.sleep(5)  # 仮の待機時間
 
-time.sleep(5)
-# ブラウザを閉じる
+# WebDriverを閉じる
 driver.close()
+
 
 dirname = "downloads"
 csv_path =get_latest_modified_file_path(dirname)
@@ -95,4 +105,7 @@ df_en_jp_names = df_en_jp_names.drop(df_en_jp_names.index[[-1]])
 datas["Name"]=df_en_jp_names
 print(datas["Name"])
 
+
+if not os.path.exists('output'):
+    os.mkdir('output')
 datas.to_csv("output/"+pack_name+"-card-ratings.csv", encoding = "shift-jis")
